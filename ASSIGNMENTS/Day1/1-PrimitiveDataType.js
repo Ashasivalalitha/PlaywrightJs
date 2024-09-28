6c2beed40db50f71a5aec8606412718faf2a5ca3
Asha <ashasiva.10002000@gmail.com>
2024-09-27 23:54:31 -0500

    added kickoff week's assignment

---
+++ ASSIGNMENTS/Day1/1-PrimitiveDataType.js
@@ -0,0 +1,22 @@
+let firstName = "fName"
+console.log(typeof(firstName));
+console.log(firstName);
+
+
+let CompanyName = 'CName'
+console.log(typeof(CompanyName));
+console.log(CompanyName);
+
+
+let mobileNumber = 9876543210
+console.log(typeof(mobileNumber));
+console.log(mobileNumber);
+
+
+let isAutomation = true
+console.log(typeof(isAutomation));
+console.log(isAutomation);
+
+let hasPlaywright
+console.log(typeof(hasPlaywright));
+console.log(hasPlaywright);
